b558fb7862acfc9c9b25d8a4b1716508ba1a028c
Priyansh1406 <57751313+Priyansh1406@users.noreply.github.com>
2021-05-22 11:43:44 +0530

    Add files via upload

---
+++ hw1.js
@@ -0,0 +1,17 @@
+function billingFunction(){
+
+	if(document.querySelector("#same").checked)
+	{
+		var name=document.querySelector("#shippingName").value;   //this will copy the value entered by the user in shipping name block
+		var zip=document.querySelector("#shippingZip").value;     //this will copy the value entered by the user in shipping zip block
+
+		document.querySelector("#billingName").value=name;   //finally these lines will copy the above initialised variables to the billing address and billing zip
+		document.querySelector("#billingZip").value=zip;
+	}
+
+	else
+	{
+		document.querySelector("#billingName").value="";
+		document.querySelector("#billingZip").value="";
+	}
+}
