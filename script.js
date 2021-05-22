3cce5440736f73a23b0828fe76210da71fb2942a
Priyansh1406 <57751313+Priyansh1406@users.noreply.github.com>
2021-05-22 11:49:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+score=0;
+cross=true;
+document.onkeydown= function(e){
+	console.log("Key code is:",e.keyCode)
+	if(e.keyCode==38){
+		dino=document.querySelector('.dino');
+		dino.classList.add('animateDino');
+		setTimeout(() =>{
+			dino.classList.remove('animateDino')
+},700);
+	}
+	if(e.keyCode==39){
+		dino=document.querySelector('.dino');
+		dinoX=parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
+		dino.style.left=dinoX+112+"px";
+	}
+	if(e.keyCode==37){
+		dino=document.querySelector('.dino');
+		dinoX=parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
+		dino.style.left=(dinoX-112)+"px";
+	}
+}
+setInterval(() =>{
+	dino=document.querySelector('.dino');
+	gameOver= document.querySelector('.gameOver');
+	obstacle=document.querySelector('.obstacle');
+	dx=parseInt(window.getComputedStyle(dino,null).getPropertyValue('left'));
+	dy=parseInt(window.getComputedStyle(dino,null).getPropertyValue('top'));
+
+	ox=parseInt(window.getComputedStyle(obstacle,null).getPropertyValue('left'));
+	oy=parseInt(window.getComputedStyle(obstacle,null).getPropertyValue('top'));
+
+	offsetX=Math.abs(dx-ox);
+	offsetY=Math.abs(dy-oy);
+	if(offsetX<93 && offsetY<52){
+		gameOver.style.visibility='visible';
+		obstacle.classList.remove('obstacleAni');
+	}	
+	else if(offsetX< 93 && cross){
+		score+=1;
+		updateScore(score);
+		cross=false;
+		setTimeout:(() =>{
+			cross=true;
+		}, 1000);
+	}
+},100);
+
+function updateScore(score){
+	scoreCont.innerHTML="Your Score: " + score
+}
